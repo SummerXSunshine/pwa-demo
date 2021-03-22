b69d552d2d7d9ee826e87b0b57bca2a60859e758
xiayirui <ssirius1998@163.com>
2021-03-22 14:21:21 +0800

    project init

---
+++ dist/js/precache-manifest.cc981246ac2885cf185573143475dcac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068c712aeacb5487a4ba",
+    "url": "/css/app.fb0c6e1c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1466c7f61c3cbe45c06b0812ea832448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "068c712aeacb5487a4ba",
+    "url": "/js/app.4190f56f.js"
+  },
+  {
+    "revision": "f9b4d57ea9851ad0c9bd",
+    "url": "/js/chunk-vendors.f01202b3.js"
+  },
+  {
+    "revision": "129e1f2f1faca97214dcf90ac4d9182a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
